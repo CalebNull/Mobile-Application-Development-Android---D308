7098cd2ba5909697da61e4486936773f24b8449a
Caleb <calebnull88@outlook.com>
2026-06-19 14:39:43 -0500

    Part B: Completed B2 - vacation detail fields 06/19/2026 (Fixed getters and setters)

--- app/src/main/java/com/example/d308/entity/Vacation.java
+++ app/src/main/java/com/example/d308/entity/Vacation.java
@@ -9,13 +9,25 @@ public class Vacation {
     @PrimaryKey(autoGenerate = true)
     private int vacationId;
     private String title;
+    private String hotelName;
+    private String startDate;
+    private String endDate;
 
-    public Vacation(String title) {
+    public Vacation(String title, String hotelName, String startDate, String endDate) {
         this.title = title;
+        this.hotelName = hotelName;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 
     public int getVacationId() { return vacationId; }
     public void setVacationId(int vacationId) { this.vacationId = vacationId; }
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
+    public String getHotelName() { return hotelName; }
+    public void setHotelName(String hotelName) { this.hotelName = hotelName; }
+    public String getStartDate() { return startDate; }
+    public void setStartDate(String startDate) { this.startDate = startDate; }
+    public String getEndDate() { return endDate; }
+    public void setEndDate(String endDate) { this.endDate = endDate; }
 }
